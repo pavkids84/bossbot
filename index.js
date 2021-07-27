2df82b8321c5b09e9033820fabebaaf7a2a63d2a
pavkids84 <87300776+pavkids84@users.noreply.github.com>
2021-07-27 17:23:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 setInterval(function () {
-  http.get("http://서버주소.herokuapp.com");
+  http.get("https://git.heroku.com/bossbot-unit.git");
 }, 60000);
 
 const Discord = require('discord.js')
